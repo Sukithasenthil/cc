8c0a64bb746405b9d60fab0ee0d7506ebeaa1180
Sukithasenthil <108688106+Sukithasenthil@users.noreply.github.com>
2023-04-05 16:19:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Header from './Day2.js/header';
+import Product from './Day2.js/product';
+import Footer from './Day2.js/footer';
+import App1 from './Componnet/PROJECT';
+function App() {
+  return (
+    <div>
+   
+    <Header/>
+    <Product/>
+    <Footer/>
+    <App1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="contact">
+        <h4>Contact Us</h4>
+        <p>25 Main Street</p>
+        <p>Gandhipuram,Coimbatore</p>
+        <p>Phone:9745453783</p>
+        <p>Email: natureess@company.com</p>
+      </div>
+      <div className="social">
+        <h4>Connect With Us</h4>
+        <ul>
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Twitter</a></li>
+          <li><a href="#">Instagram</a></li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
